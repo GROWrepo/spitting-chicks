b5ce827f748478128e175ecfb789ecea434fe70a
sentendro <ghu333@hanmail.net>
2017-08-04 15:26:50 +0900

    restore add behavior of player1 when it on SUPERJUMP

--- Assets/script/Player1.cs
+++ Assets/script/Player1.cs
@@ -16,13 +16,19 @@ public class Player1 : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (this.SP == STATUS_PLAYER.IDLE)
+        if (this.SP == STATUS_PLAYER.IDLE && this.SP == STATUS_PLAYER.SUPERJUMP)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
                 this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, self.getJump()));
                 this.GetComponent<Animator>().SetBool(this.GetComponent<Animator>().parameters[1].name, true);
                 this.SP = STATUS_PLAYER.JUMPING;
+                if (this.SP == STATUS_PLAYER.IDLE)
+                {
+                    this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, self.getJump()));
+                    this.GetComponent<Animator>().SetBool(this.GetComponent<Animator>().parameters[1].name, true);
+                    this.SP = STATUS_PLAYER.JUMPING;
+                }
             }
             else if (Input.GetKey(KeyCode.D))
             {
@@ -145,7 +151,8 @@ public class Player1 : MonoBehaviour {
     private void onSuperJump()
     {
         this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1.6f * this.self.getJump()));
-        SP = STATUS_PLAYER.JUMPING;
+        this.GetComponent<Animator>().SetBool(this.GetComponent<Animator>().parameters[1].name, true);
+        SP = STATUS_PLAYER.SUPERJUMP;
     }
 
     private void onGround()
