3ed473dee7a2084aca659fec4583028f76b2ff2f
sentendro <ghu333@hanmail.net>
2017-08-04 14:51:15 +0900

    "restore old script folder"
    This reverts commit 1ba0de0834ca3c105adb95fe49f5535eef5b56d4.

---
+++ Assets/script/watermelon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class watermelon : MonoBehaviour {
+    bool isDown = false;
+    int capacity;
+	// Use this for initialization
+	void Start () {
+        this.capacity = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject);
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.SendMessage("getSeed", this.capacity);
+                this.isGotten();
+            }
+        }
+    }
+    */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log(collision.gameObject);
+            collision.gameObject.GetComponent<Transform>().parent.gameObject.SendMessage("getSeed", this.capacity);
+            this.isGotten();
+        }
+    }
+    private void isGotten()
+    {
+        this.capacity += 5;
+        this.gameObject.SetActive(false);
+    }
+}
