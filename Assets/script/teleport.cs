d7413bb460d52571b5e539217df6c7686f563207
castlesu <shsj4952@naver.com>
2017-08-05 20:54:38 +0900

    create teleport

---
+++ Assets/script/teleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour {
+    public float speed;
+    public Vector2 left, right;
+    public bool isTele;
+    // Use this for initialization
+    void Start () {
+        left = this.gameObject.GetComponent<Transform>().localPosition;
+        right = new Vector2(0, 10);
+        speed = 4.0f; 
+        isTele = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isTele = true;
+        }
+        if (isTele)
+        {
+            this.gameObject.GetComponent<Transform>().Translate(0, 10, 0);
+            isTele = false;
+        }
+	}
+}
